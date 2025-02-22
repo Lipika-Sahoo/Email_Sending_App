e7e7434715037f931b72a624ad8f98114d466c52
Lipika-Sahoo <lipikasahoo728@gmail.com>
2025-02-22 17:43:24 +0530

    Add files via upload

---
+++ EmailSendingAppApplication.java
@@ -0,0 +1,16 @@
+package com.becoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendingAppApplication.class, args);
+	}
+
+
+}
+
+
